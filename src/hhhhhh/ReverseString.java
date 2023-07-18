0e7b25f33a53ad402a2fc9fae45be0b519247a81
hanmantnature <hanmantnature990@gmail.com>
2023-07-18 11:41:56 +0530

    Create ReverseString

---
+++ src/hhhhhh/ReverseString.java
@@ -0,0 +1,21 @@
+package hhhhhh;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		 {
+		        String input = "InfowayTechnology";
+		 
+		        StringBuilder input1 = new StringBuilder();
+		 
+		       
+		        input1.append(input);
+		 
+		       
+		        input1.reverse();
+		 
+		        
+		        System.out.println(input1);
+		    }
+	     }	
+	}
